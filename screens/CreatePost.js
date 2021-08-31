c478a7b449a68de69fdddff847833c34749f8454
Advithiwhitehat <74882349+Advithiwhitehat@users.noreply.github.com>
2021-08-30 19:02:00 -0700

    Add files via upload

---
+++ screens/CreatePost.js
@@ -0,0 +1,11 @@
+import { View, Text} from "react-native";
+import React from "react";
+export default class CreatePost extends Component{
+    render(){
+        return(
+            <View>
+                <Text>Create Post Screen</Text>
+            </View>
+        );
+    }
+}
---
+++ screens/Feed.js
@@ -0,0 +1,11 @@
+import { View, Text} from "react-native";
+import React from "react";
+export default class Feed extends Component{
+    render(){
+        return(
+            <View>
+                <Text>Feed Screen</Text>
+            </View>
+        );
+    }
+}
